5f21976248bde7ffeb8a29487c28c827e2d5fa7c
bin <45528125+seobinbang7@users.noreply.github.com>
2023-12-13 06:33:28 +0900

    docs: routes.jsx path 부분 수정

--- src/routes.jsx
+++ src/routes.jsx
@@ -13,7 +13,7 @@ import EditProfile from './pages/EditProfile/EditProfile';
 
 const router = createHashRouter([
   {
-    path: '/lumieleu',
+    path: '/',
     element: <RootLayout />,
     children: [
       { index: true, element: <Home /> },
