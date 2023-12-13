3da535172e183296094ffc3a9e67de26353cf14e
bin <45528125+seobinbang7@users.noreply.github.com>
2023-12-13 12:49:12 +0900

    docs: '/'로 수정

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -259,7 +259,7 @@ function OrderList() {
             'aria-live': 'polite',
           },
         });
-        navigate('/lumieleu/mypage');
+        navigate('/mypage');
       } catch (error) {
         console.error(error);
         toast.error('결제에 실패했습니다', {
